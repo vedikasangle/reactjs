19ac4b36c694d46638d5a5f4f9de3f62661ef0b5
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-12-08 18:47:15 +0530

    Create Math.js

---
+++ importexportines6/Math.js
@@ -0,0 +1,13 @@
+//math.js is a module
+const pi = 3.1415962;
+
+function doublePi() {
+  return pi * 2;
+} //double pie is a function
+
+function triplePi() {
+  return pi * 3;
+} //triple pie is a function
+
+export default pi;
+export { doublePi, triplePi };
