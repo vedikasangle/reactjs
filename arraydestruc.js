85aa69e4b3f748059548c3fb5edbc4bc69d42904
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-12-18 22:22:01 +0530

    Create arraydestruc.js

---
+++ arraydestruc.js
@@ -0,0 +1,32 @@
+import animals from "./data";
+
+// console.log(animals);
+
+const [cat, dog] = animals;
+  
+const { name: catname, sound: catsound } = cat;
+console.log(catsound);
+
+// CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
